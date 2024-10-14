683ba4e65de3b878925cec627d1742ce83ed480e
Adham-16 <a98347612z@gmail.com>
2024-10-14 04:16:12 +0300

    update in Brands page

--- src/Brands/Brands.jsx
+++ src/Brands/Brands.jsx
@@ -1,34 +1,32 @@
-
 import axios from "axios"
-import { useEffect, useState } from "react";
 import { LoadingScreen } from "../LoadingScreen/LoadingScreen";
+import { useQuery } from "@tanstack/react-query";
+import { NotFound } from "../notfound/notfound";
 
 
 export function Brands() {
-  const [Brands, setBrands] = useState([]);
-  const [IsLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    getBrands()
-  }, []);
+  
   async function getBrands() {
-    setIsLoading(true)
     let {data} = await axios.get('https://ecommerce.routemisr.com/api/v1/brands')
-      setBrands(data.data)
-      console.log(data);
-      
-    setIsLoading(false)
-    
+    return data.data;
   }
+
+    const { data: brands, isLoading, error } = useQuery({
+    queryKey: ['brands'],
+    queryFn: getBrands
+  });
+
+    if (isLoading) return <LoadingScreen />;
+  if (error) return <NotFound/>;
+
   return (
     <>
-    {IsLoading ? <LoadingScreen /> :
     <div  className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 " >
-        {Brands?.map((Brand,index) =>{
+        {brands?.map((Brand,index) =>{
             return <div key={index}>
                 <div className="w-full h-full bg-[#0000008c] border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <div className="flex justify-center">
-                        <img className="p-2 rounded-t-lg  max-h-56 w-full" src={Brand.image} alt="product image" />
+                        <img className="p-2 rounded-t-lg  max-h-56 w-full" src={Brand.image} alt={Brand.name + " Logo"} />
                     </div>
                     <div className="px-5 pb-5">
                         <div>
@@ -39,7 +37,6 @@ export function Brands() {
             </div>
         })}
     </div>
-    }
     </>
   )
 }
