7f1c699830b470ef413403fec73a1297bfc7021b
Adham Mohamed <85314873+Adham-16@users.noreply.github.com>
2024-11-15 16:40:52 +0300

    Update ProductDetails.jsx

--- src/ProductDetails/ProductDetails.jsx
+++ src/ProductDetails/ProductDetails.jsx
@@ -75,7 +75,7 @@ export function ProductDetails() {
                 </div>
             </div>
             <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
-              <a href="#" title className="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-[#434445] focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700   dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" role="button">
+              <a onClick={() => addProductToWishlist(product._id)} href="#" title className="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-[#434445] focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700   dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" role="button">
                 <svg className="w-5 h-5 -ms-2 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" viewBox="0 0 24 24">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z" />
                 </svg>
